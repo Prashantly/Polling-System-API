a9f887e413147a05e6cddaf22bac4613544356b1
Prashant <prashantyallatti79968@gmail.com>
2023-06-11 09:50:55 +0530

    install mongoose and configure mongoose in config folder

---
+++ config/mongoose.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+let db;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(url, connectionParams);
+    db = mongoose.connection;
+    console.log("Database connection successfull.");
+  } catch (err) {
+    console.log(err);
+    console.error("Error in connecting to database!!!");
+  }
+};
+
+connectDB();
+module.exports = db;
