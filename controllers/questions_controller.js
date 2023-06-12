2c9d063230e02ea2b3820beffc107192bf26e054
Prashant <prashantyallatti79968@gmail.com>
2023-06-12 08:07:08 +0530

    Setup routes for delete question and implemented delete question and all its associated options

--- controllers/questions_controller.js
+++ controllers/questions_controller.js
@@ -25,7 +25,7 @@ module.exports.create = async (req, res) => {
   }
 };
 
-//get particulare question by id and all its's options
+//get particular question by id and all its's options
 module.exports.getQuestion = async (req, res) => {
   try {
     const { questionId } = req.params;
@@ -50,6 +50,45 @@ module.exports.getQuestion = async (req, res) => {
     res.status(500).send({
       message:
         err.message || "Some error occurred while retrieving the question.",
+    });
+  }
+};
+
+// delete question
+module.exports.deleteQuestion = async (req, res) => {
+  try {
+    const { questionId } = req.params;
+    //find question by id
+    const question = await Question.findById(questionId).populate("options");
+
+    //if question does not exist
+    if (!question) {
+      return res.status(404).json({
+        message: "Question not found with id " + questionId,
+      });
+    }
+
+    //if question exist then delete question
+    const deletedQuestion = await Question.deleteOne({
+      _id: questionId,
+    });
+
+    //delete all options associated with given question
+    const deletedOptions = await Option.deleteMany({
+      questionRef: questionId,
+    });
+
+    //if question is deleted successfully send the response
+    return res.status(200).json({
+      message: "Question deleted successfully",
+      deletedQuestion,
+      deletedOptions,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message:
+        err.message || "Some error occurred while deleting the question.",
     });
   }
 };
--- routes/question.js
+++ routes/question.js
@@ -6,9 +6,16 @@ const router = express.Router();
 const QuestionsController = require("../controllers/questions_controller");
 const OptionsController = require("../controllers/options_controller");
 
+//get particular quastion by it's id
 router.get("/:questionId", QuestionsController.getQuestion);
+
+//create question
 router.post("/create", QuestionsController.create);
 
+//create option for particular question
 router.post("/:questionId/options/create", OptionsController.createOption);
+
+//delete question by it's id
+router.delete("/:questionId/delete", QuestionsController.deleteQuestion);
 
 module.exports = router;
